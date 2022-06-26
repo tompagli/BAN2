faefbec3d06c848c4ebf0778c4558e307222c4b4
tompagli <pagliuso.tom@gmail.com>
2022-06-26 00:52:02 -0300

    ident

--- functions.py
+++ functions.py
@@ -38,15 +38,25 @@ def insert(table):
     if table== 2 : ##tocar
         cod_int = int(input())
         cod_mus = int(input())
+
         cursor.execute("INSERT INTO tocar VALUES(%s,%s)", (cod_int,cod_mus))
         connection.commit()
+
+        tocar_instance = tocar(cod_int,cod_mus)
+        lista_tocar.append(tocar_instance)
+
     if table== 3 : ##musica
         titulom = str(input())
         autor = str(input())
         cod_mus = int(input())
         cod_dis = int(input())
+
         cursor.execute("INSERT INTO Musica VALUES(%s,%s,%s,%s)", (titulom,autor,cod_mus,cod_dis))
         connection.commit()
+
+        musica_instance = musica(titulom,autor,cod_mus,cod_dis)
+        lista_musica.append(musica_instance)
+
     if table == 4 : ##disco
         titulod = str(input())
         data = strftime(input())
@@ -54,32 +64,57 @@ def insert(table):
         formato = str(input())
         cod_mus = int(input())
         cod_b = int(input())
+
         cursor.execute("INSERT INTO Disco VALUES(%s,%s,%s,%s,%s,%s)", (titulod,data,id,formato,cod_mus,cod_b))
         connection.commit()
+
+        disco_instance = disco(titulod,data,id,formato,cod_mus,cod_b)
+        lista_disco.append(disco_instance)
+
     if table == 5 : ##musico
         nome = str(input())
         n_regs = int(input())
         num_tel = int(input())
         n_casa = int(input())
+
         cursor.execute("INSERT INTO Musico VALUES(%s,%d,%d,%d)", (nome,n_regs,num_tel,n_casa))
         connection.commit()
+
+        musico_instance = musico(nome,n_regs,num_tel,n_casa)
+        lista_musico.append(musico_instance)
+
     if table == 6 : ## endereco
         casa = int(input())
         telefone = int(input())
+
         cursor.execute("INSERT INTO Endereco VALUES(%d,%d)", (casa,telefone))
         connection.commit()
+
+        endereco_instance = endereco(casa,telefone)
+        lista_endereco.append(endereco_instance)
+
     if table == 7 : ## banda 
         codb = int(input())
         cod_mus = int(input())
         nome = str(input())
+
         cursor.execute("INSERT INTO Banda VALUES(%d,%d,%s)",(codb,cod_mus,nome))
         connection.commit()
+
+        banda_instance = banda(codb,cod_mus,nome)
+        lista_banda.append(banda_instance)
+
     if table == 8 : ## produtor
         cod_mus = int(input())
         cod_dis = int(input())
         codp = int(input())
+
         cursor.execute("INSERT INTO Produtor VALUES (%d,%d,%d)",(cod_mus,cod_dis,codp))
         connection.commit()
+
+        produtor_instance = produtor(cod_mus,cod_dis,codp)
+        lista_produtor.append(produtor_instance)
+        
     connection.close()
 
 def view(table):
