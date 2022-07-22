e95e735168b6a2d9d97ea73a2f28c5758b96a521
tompagli <pagliuso.tom@gmail.com>
2022-07-22 20:35:08 -0300

    major updates on functions i believe insert is working

--- BAN2-T2/connection.py
+++ BAN2-T2/connection.py
@@ -5,8 +5,8 @@ def connect_db():
     from pymongo import MongoClient
     import pymongo
 
-    CONNECTION_STRING = "mongodb://localhost:27017/gravadora"
+    CONNECTION_STRING = "mongodb://localhost:27017"
 
     from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
-
+    return client['gravadora']
